7cbf5cf3ccedd56e9a793b6db033d58f1365f975
vnogueira91 <vitor.nogueira@pdmfc.com>
2025-05-06 13:15:40 +0100

    refs #2 - added methods to builder

--- src/fabric/fabric-ca-server/constants.ts
+++ src/fabric/fabric-ca-server/constants.ts
@@ -11,3 +11,14 @@ export enum FabricCAServerDBType {
   POSTGRES = "postgres",
   MYSQL = "mysql",
 }
+
+export enum FabricCAServerCurveName {
+  FP256BN = "amcl.Fp256bn",
+  BN254 = "gurvy.Bn254",
+  FP256MIRACLBN = "amcl.Fp256Miraclbn",
+}
+
+export enum FabricCAServerEnrollmentType {
+  X509 = "x509",
+  IDEMIX = "idemix",
+}
