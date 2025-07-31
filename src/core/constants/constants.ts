4e93852ac9260cfb6a0556be0897bdc3e12d199c
vnogueira91 <vitor.nogueira@pdmfc.com>
2025-07-31 17:01:57 +0100

    DECAF-168 - added logic to compile basic contracts

--- src/core/constants/constants.ts
+++ src/core/constants/constants.ts
@@ -2,3 +2,7 @@ export const COMMA_SEPARATOR = ",";
 export const COLON_SEPARATOR = ":";
 export const PIPE_SEPARATOR = "|";
 export const SLASH_SEPARATOR = "/";
+
+export const NAME_PLACEHOLDER = "##NAME##";
+export const VERSION_PLACEHOLDER = "##VER##";
+export const ENTRY_PLACEHOLDER = "##ENTRY##";
