85ac1bf4463777704f429f98a55a0d214ad6a651
vnogueira91 <vitor.nogueira@pdmfc.com>
2025-10-14 18:21:54 +0100

    DECAF-168 - adjusted code to easily generate client config

--- tests/integration/compile-contract.test.ts
+++ tests/integration/compile-contract.test.ts
@@ -1,5 +1,6 @@
 import { execSync } from "child_process";
-
+import { FabricCAClientConfigBuilder } from "../../src";
+import path from "path";
 // Model.setBuilder(Model.fromModel);
 
 jest.setTimeout(50000);
@@ -17,5 +18,23 @@ describe.skip("Test test model contract", () => {
   it("should validate a TestModel instance", () => {
     console.log("Test model contract");
     console.log("Validate a TestModel instance");
+  });
+
+  it("tests", async () => {
+    const builder = new FabricCAClientConfigBuilder();
+
+    builder
+      .setCustom("bccsp", {
+        default: "PKCS11",
+        pkcs11: {
+          Library: "/usr/lib/softhsm/libsofthsm2.so",
+          Pin: 1213213,
+          Label: "lasdasdasd",
+          hash: "SHA2",
+          security: 256,
+          Immutable: false,
+        },
+      })
+      .save(path.join(__dirname));
   });
 });
