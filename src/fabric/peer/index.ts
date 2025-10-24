dd9f9a518ddb2178cc57e17855d5abb273419d16
bagacito <83371993+bagacito@users.noreply.github.com>
2025-10-24 11:29:01 +0100

    Development (#36)
    * DECAF-168 - adjusted code to easily generate client config
    
    * DECAF-168 - adjusted code to easily generate client config
    
    * DECAF-168 - git status
    
    ---------
    
    Co-authored-by: vnogueira91 <vitor.nogueira@pdmfc.com>

--- src/fabric/peer/index.ts
+++ src/fabric/peer/index.ts
@@ -1,3 +1,4 @@
 export * from "./fabric-peer-command-builder";
 export * from "./fabric-peer-config-builder";
 export * from "./fabric-peer-node-command-builder";
+export * from "./fabric-peer-channel-command-builder";
