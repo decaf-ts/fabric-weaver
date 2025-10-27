026e5754b39be097f0b730b84efc17e04b4c309e
vnogueira91 <vitor.nogueira@pdmfc.com>
2025-10-27 11:02:48 +0000

    DECAF-168 - exposed more builders

--- src/fabric/peer/index.ts
+++ src/fabric/peer/index.ts
@@ -2,3 +2,4 @@ export * from "./fabric-peer-command-builder";
 export * from "./fabric-peer-config-builder";
 export * from "./fabric-peer-node-command-builder";
 export * from "./fabric-peer-channel-command-builder";
+export * from "./fabric-peer-lifecycle-chaincode-command-builder";
